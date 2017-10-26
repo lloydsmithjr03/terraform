235bfcd1e9991c88d1d848be0e9721f52c730810
Lloyd Smith Jr <lloydsmithjr03@gmail.com>
2017-10-25 21:58:31 -0400

    Removed app service
    Signed-off-by: Lloyd Smith Jr <lloydsmithjr03@gmail.com>

--- web.tf
+++ web.tf
@@ -45,17 +45,3 @@ resource "azurerm_app_service" "lsmith-terraform-2" {
         use_32_bit_worker_process = false
     }
 }
-
-resource "azurerm_app_service" "lsmith-terraform-3" {
-    name = "lsmith-terraform-3"
-    location  = "${azurerm_resource_group.terraform.location}"
-    resource_group_name = "${azurerm_resource_group.terraform.name}"
-    app_service_plan_id = "${azurerm_app_service_plan.terraform_apps.id}"
-
-    site_config {
-        dotnet_framework_version = "v4.0"
-        local_mysql_enabled = false
-        websockets_enabled = false
-        use_32_bit_worker_process = false
-    }
-}
